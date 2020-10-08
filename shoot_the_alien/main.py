a40e1ca4887728f2cb51aeef487df05263be61bd
QAMilestoneAcademy <48306511+QAMilestoneAcademy@users.noreply.github.com>
2020-10-08 08:49:19 +0400

    test

---
+++ shoot_the_alien/main.py
@@ -0,0 +1,32 @@
+import pgzrun
+from random import randint
+
+TITLE='Shooting The Alien'
+WIDTH = 800
+HEIGHT = 600
+message=""
+
+alien = Actor('alien')
+# alien.pos = 50, 50
+#pygamezero function
+def draw():
+    screen.clear()
+    screen.fill(color=(128,0,0))
+    alien.draw()
+    screen.draw.text(message,center=(400,40),fontsize=30)
+
+def place_alien():
+    alien.x=randint(50,WIDTH-50)
+    alien.y=randint(50,HEIGHT-50)
+
+def on_mouse_down(pos):
+    global message
+    if alien.collidepoint(pos):
+        message='Good Shot'
+        place_alien()
+    else:
+        message='You missed'
+#
+# place_alien()
+
+pgzrun.go()
